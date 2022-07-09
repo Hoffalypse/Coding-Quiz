ea673d7125483d21dde321922d01d82509382153
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-07-08 23:34:39 -0600

    Html base code and variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,16 @@
-var 
+
+
+
+//start button event clears page and asks first question
+
+//start a timer for questions 
+// end timer 
+
+//log correct answers and doc time for incorrect
+//use conditinal operators (see notes) for actions pertaining to right/wrong answers
+
+//ask for player initials
+
+//show leaderboard
+
+//ask to play again
